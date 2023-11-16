fdb26ef4a09eb1e22157e2f92526be2fd253ac9f
dnalverson <88463574+dnalverson@users.noreply.github.com>
2023-11-16 15:47:40 -0500

    making pip installable on local

--- IsoDAT2D/setup.py
+++ IsoDAT2D/setup.py
@@ -8,6 +8,6 @@ setup(
     long_description= 'Combining non-negative matrix factorization and clustering algorithms to process thin film X-ray total scattering data and separate isotropic and anisotropic components',
     author_email='dalverson@ufl.edu',
     packages = ['IsoDAT2D',],
-    url='https://github.com/dnalverson/ml-for-thinfilmdata,
+    url='https://github.com/dnalverson/ml-for-thinfilmdata',
     install_requires = ['matplotlib', 'pyFAI', 'scipy', 'numpy', 'scikit-learn']
 )
