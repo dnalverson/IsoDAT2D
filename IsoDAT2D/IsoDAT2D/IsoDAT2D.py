3702c3d4b8ce3cbf75a874f67a04213dbbb473e1
dnalverson <88463574+dnalverson@users.noreply.github.com>
2024-07-23 10:50:14 -0400

    updating IsoDAT2D

--- IsoDAT2D/IsoDAT2D/IsoDAT2D.py
+++ IsoDAT2D/IsoDAT2D/IsoDAT2D.py
@@ -4,200 +4,231 @@
 # This software is licensed under the BSD 3-Clause License.
 # See the LICENSE file in the project root for details.
 
-import pandas as pd 
-import numpy as np
+import SimDAT2D as sim
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd 
 from sklearn.decomposition import NMF
-from sklearn.preprocessing import StandardScaler
-import csv
 import dask
 import glob as glob
 from tifffile import imread, imshow
 import warnings
-import os
-from dask import compute, delayed
-
+import dask
 
 def attempt(Real_Data, Length, i, init= None, solver = 'cd', beta_loss = 'frobenius', iter = 500):
-    # Create an NMF model with specified parameters
     NMF_model = NMF(n_components=i, init = init, solver = solver, beta_loss = beta_loss, max_iter = iter)
-    
-    # Fit the NMF model to the input data
     NMF_data= NMF_model.fit_transform(Real_Data)
-    
-    # Extract the factorized matrix components
     fit_compos = NMF_model.components_
-    
-    # Calculate the reconstruction error of the model
     Q = np.array(NMF_model.reconstruction_err_)*100
-    
-    # Suppress warnings from the library
     warnings.filterwarnings("ignore", category=FutureWarning)
-    
-    # Return the reconstruction error, factorized matrix components, and transformed data
+    return Q
+
+def attempt2(Real_Data, Length, i, init= None, solver = 'cd', beta_loss = 'frobenius', iter = 500):
+    NMF_model = NMF(n_components=i, init = init, solver = solver, beta_loss = beta_loss, max_iter = iter)
+    NMF_data= NMF_model.fit_transform(Real_Data)
+    fit_compos = NMF_model.components_
+    Q = np.array(NMF_model.reconstruction_err_)*100
+    warnings.filterwarnings("ignore", category=FutureWarning)
     return Q, fit_compos, NMF_data
 
-def Run_NMF(Real_Data, init= None, solver = 'cd', beta_loss = 'frobenius', itear = 1000):
-    """ 
-    This function performs NMF (Non-negative Matrix Factorization) on the input data, Real_Data.
-    The function takes in the following parameters:
-    Real_Data: the input data to be factorized
-    init: the initial guess of the factorization (default is None)
-    solver: the solver to be used in the factorization (default is 'cd' - Coordinate Descent)
-    beta_loss: the beta-divergence loss function to be used (default is 'frobenius')
-    itear: the number of iterations to perform in the factorization (default is 1000)
 
-    The function returns a tuple of two items:
-        1. A pandas dataframe of the NMF components
-        2. The reconstructed NMF data
-        
-    The function uses Dask for parallel computing, and opens a web browser to display the Dask dashboard. 
-    """
-    import dask.delayed
-    from dask import compute, delayed
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
+def Run_NMF(Real_Data, init= None, solver = 'cd', beta_loss = 'frobenius', itear = 1000, show = False):
+    
+    """" Comparison of multiple components without manually comparing
+    multiple components all at once. There are a few ways that this can be done. One way
+    is to take the residuals of the datasets with themselves to see which is the closests to the 
+    "correct" component. This may only be done on sample data potentially. Could include a 
+    simulated dataset of what the standing component should look like give or take some 
+    variations in the data. When the residuals are taken if it is less than some given 
+    percentage the would be the dataset/NMF component to use further. There may be multiple
+    thus having to go in manually to find differences. 
+    
+    To do this, will need to import the simulated XRD pattern from VESTA and then take the residual differences
+    of each component and compared to the simulated pattern. Will have the program spit out the compnents that
+    meet the cutoff. """
 
-    from dask.distributed import Client
-    client = Client()
-    import webbrowser
-    #webbrowser.open(client.dashboard_link)
-
-    # Initialize variables
     In = init
     Solve = solver
     Beta = beta_loss
     It = itear
+    
 
-    # Delayed computation of the function attempt for each number of components
     jobs = [dask.delayed(attempt)(Real_Data, Real_Data.shape[1], i, In, Solve, Beta, It) for i in range(1, Real_Data.shape[1])]
+    #jobs = [dask.delayed(attempt)(Real_Data, Real_Data.shape[1], i, In, Solve, Beta, It) for i in range(1,20)]
     calcs = dask.compute(jobs)[0]
 
-    # Convert the results to a numpy array
     calcs = np.array(calcs)
-
-    # Find the minimum beta-divergence between the training data and the reconstructed data
     min_Q = np.min(calcs)
     noc = np.where(calcs == min_Q)
     noc_2 = noc[0]
-    number_of_components = noc_2[0] + 1
+    number_of_components = noc_2[0] +1
 
-    # Run the final NMF with the number of components found
-    Divergence, compos, NMF_Data_2 = attempt(Real_Data, Real_Data.shape[1], number_of_components, In, Solve, Beta)
-
-    # Print the beta-divergence and number of components used
-    print('The beta-divergence between the training data and reconstructed data is',
-            Divergence,'%', 'The final number of components used were', number_of_components + 1, '/n')
-
-    # Create a pandas dataframe of the NMF components
-    components_dataframe = pd.DataFrame(compos).T
-
-    # Plot the NMF components
-    plt.figure(figsize=(10, 10))
-    plt.plot(components_dataframe, linewidth=2, alpha=0.7, c='k')
-    plt.title('NMF Components')
-    plt.ylabel('Intensity')
-    plt.xlabel('Q')
-
-    return components_dataframe, NMF_Data_2
+    Divergence, compos, NMF_Data_2 = attempt2(Real_Data, Real_Data.shape[1], number_of_components, In, Solve, Beta)
+            
+    
+    print('The beta-divergence is: ', Divergence, '%\n','The final number of components used were',number_of_components+1, '\n') 
+    
+    
+    m = pd.DataFrame(compos)
+    m = m.T
+    
+    if show == True:
+        plt.figure(figsize = (5,5))
+        colors = plt.cm.magma(np.linspace(0,1, number_of_components))
+        i = 0
+        while i < number_of_components:
+            plt.plot(m[i], c = colors[i], alpha = 0.7)
+            i = i+1
+    
+    return m,NMF_Data_2, min_Q
 
 def AggCluster(Number_Clusters, data):
     
-    """
-    A program that will take in the type of scikitlearn clustering algorithm
-    desired and the number of clusters as well as the data in a numpy array
-    and output the associated clusters with the original data. This will make
-    the 'latent' space from the clustering algorithms have more meaning
-    
-    Input:
-    Number_Clusters - int, the number of clusters desired
-    data - numpy array, the data to be used for clustering
-    
-    Output:
-    Understanding_data - dictionary, contains the cluster number and the associated
-                        int_angle value
-    """
-    
-    # Import the AgglomerativeClustering function from the scikit-learn library
+    """A program that will take in the type of scikitlearn clustering algorithm
+        desired and the number of clusters as well as the data in a numpy array
+        and output the associated clusters with the original data. This will make
+        the 'latent' space from the clustering algorithms have more meaning"""
+
     from sklearn.cluster import AgglomerativeClustering
-    
-    # Create an AgglomerativeClustering object with the desired number of clusters
-    Make_Clusters = AgglomerativeClustering(n_clusters=Number_Clusters, compute_distances=True)
-    
-    # Fit the model to the data and predict the cluster labels
+    Make_Clusters= AgglomerativeClustering(n_clusters = Number_Clusters, compute_distances=True)
     y_kmeans = Make_Clusters.fit_predict(data)
-    
-    # Fit the model to the data
     information = Make_Clusters.fit(data)
-    
-    # Get the distances parameter
     parameter = information.distances_
 
-    # Initialize the variables for looping
     x = 0
-    Understanding_data = {"Cluster_Number":[], "Int_Angle":[]}
-    
-    # Loop through each data point and add the cluster number and int_angle value to the dictionary
+    Understanding_data = {"Cluster_Number":[], "Int_Angle":[]};
     while x < len(data):
         Understanding_data["Cluster_Number"].append(y_kmeans[x])
         Understanding_data["Int_Angle"].append(data[x])
         x = x+1
         
-    # Loop through each cluster and create a separate plot for each cluster
+    # Create an empty list to store the data
+    data_list = []
+    
+        
     q = 0
     while q < Number_Clusters:
         z = 0
-        plt.figure()
+        plt.figure(figsize=(5,5))
         plt.xlabel('X')
         plt.ylabel('Y')
-        plt.title('Agglomerative Clustering' + ' ' + str(q))
+        plt.title('Agglomerative Clustering'+' ' +str(q))
 
-        # Loop through each data point and plot only the points belonging to the current cluster
         while z < len(data):
             if Understanding_data["Cluster_Number"][z] == q:
-                plt.plot(Understanding_data["Int_Angle"][z], label='Component' + str(z))
+                plt.plot(Understanding_data["Int_Angle"][z], label = 'Component'+str(z))
+            z = z+1
+    
+        # Check if the plot looks good if the plot looks good, append the data to the list
+        plt.show(block = False)
+        plt.pause(0.1)
+        
+        if input("Do the identified components look like an isotropic scattering signal? (y/n)") == 'y':
+            i = 0
+            while i < len(data):
+                if Understanding_data["Cluster_Number"][i] == q:
+                    data_list.append(Understanding_data["Int_Angle"][i])
+                i = i+1
                 
-            z = z + 1
-        q = q + 1
-        
-    # Return the dictionary of cluster numbers and int_angle values
+        q = q+1
     
-    return Understanding_data
+    return Understanding_data, data_list
 
-def Combine_Cluster(Understanding_data):
-    """
-        A program that will take in a user specified cluster or clusters of choice, from the Agglomerative 
-        cluster function and average the signals for one array of data. This data will then be
-        smoothed in a different function called smooth_data.
+
+def smooth_components(Identified_components, filter_strength = 2, show = False):
+    '''A function that will smooth the components identified from the agglomerative clustering algorithm'''
+    
+    # Importing required library
+    from scipy.signal import savgol_filter
+    
+    # Defining a dictionary that maps filter strength to the number of points for the smoothing window
+    strength_to_points = {1: 3, 2: 5, 3: 7, 4: 11, 5: 15}
+    
+    # Retrieving the number of points for the smoothing window based on the filter strength provided
+    points = strength_to_points.get(filter_strength)
+
+    # Applying Savitzky-Golay filter to smooth the identified components by taking their average along the columns
+    smoothed_compos = savgol_filter(np.mean(Identified_components, axis = 0), points, 1)
+    
+    # If show argument is True, plotting the original components, their average, and the smoothed components
+    if show == True:
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(10, 10), sharex=True)
+        ax1.plot(Identified_components.T, c = 'k', linewidth = 2, alpha = 0.7)
+        ax1.set_title('Identified Components')
+        ax1.set_ylabel('Intensity')
+        ax2.plot(np.mean(Identified_components, axis = 0), c = 'r', linewidth = 2, alpha = 0.7)
+        ax2.set_ylabel('Intensity')
+        ax2.set_title('Mean of Identified Components')
+        ax3.plot(smoothed_compos, c = 'g', linewidth = 2, alpha = 0.7, label = 'Smoothed Component')
+        ax3.scatter(np.arange(len(np.mean(Identified_components, axis = 0))), np.mean(Identified_components, axis = 0), c = 'r', s = 10, 
+                    label = 'Mean Component')
+        ax3.set_xlabel('Data Points')
+        ax3.set_title('Smoothed Component')
+        ax3.set_ylabel('Intensity')
+        ax3.legend()
+    
+    # Returning the smoothed components
+    return smoothed_compos
+
+def run_nmfac(Data, initialize_iter = 0, clusters = 5):
+    """A function that will run the NMF algorithm and then cluster with agglomerative clustering the components and returns the 
+        identified components for later PDF analysis. The function starts with a random initializer
+        that will be used to initialize the NMF algorithm. The user can decide how many iterations the 
+        initializer takes. Then it will go through the NMF algorithm and compare the beta divergences of all the
+        initializations and select the one with the lowest. By default, there is no initializer and the NMF algorithm
+        uses preset parameters to run the algorithm. """
         
-        Input: 
-        Understanding_data - dictionary, contains the cluster number and the associated
+        #NMF Parameter Values
         
-        Output:
-        Identified_signal - averaged numpy array, the averaged signal from the cluster of interest
-    """
-    cluster_of_interest = []
-    keep_going = True
-    cluster_numbers = np.array(Understanding_data['Cluster_Number'])
+    import warnings
+    warnings.filterwarnings("ignore")
     
-    while keep_going == True:
-        print('Please enter the cluster number of interest')
-        for i in range(0, len(cluster_numbers)):
-            print('Cluster ', i+1)
-        selected_cluster = int(input("Enter the cluster number of interest: ")) - 1
-        cluster = np.where(cluster_numbers == selected_cluster)
-        cluster_data = cluster[0]
-        cluster_of_interest.append(cluster_data)
-        keep_going = bool(input("Would you like to add another cluster? (True/False)"))
     
-    arrayed_cluster = np.array(cluster_of_interest)
-    Identified_signal = np.mean(arrayed_cluster, axis = 0)
-    
-    plt.figure(figsize = (10,10))
-    plt.plot(Identified_signal)
-    plt.x_label('Q')
-    plt.ylabel('Intensity')
-    plt.title('Averaged Signal')
-    
-    return Identified_signal
+    init_params = ['random', 'nndsvd', 'nndsvda', 'nndsvdar']
+    solver_params = ['cd', 'mu']
+    beta_loss_params = ['frobenius', 'kullback-leibler']
+    tol_params = np.arange(0.00001, 0.01, 0.0001)
+    max_iter_params = np.arange(100, 10000, 100)
+    shuffle_params = [True, False]
+        
+    #When random initializers are not wanted
+    if initialize_iter == 0:
+        weights, components, beta = Run_NMF(Data, show = True)
+        AggComponents = np.array(components).T
+        AggClusters, found_compos = AggCluster(clusters, AggComponents)
+        
+        found_compos = np.array(found_compos)
+        
+    elif initialize_iter > 0:
+        beta_div = []
+        
+        init_for_init = np.random.choice(init_params,initialize_iter)
+        solver_for_init = np.random.choice(solver_params, initialize_iter)
+        beta_loss_for_init = np.random.choice(beta_loss_params, initialize_iter)
+        tol_for_init = np.random.choice(tol_params, initialize_iter)
+        max_iter_for_init = np.random.choice(max_iter_params, initialize_iter)
+        shuffle_for_init = np.random.choice(shuffle_params, initialize_iter)
+        
+        for i in range(initialize_iter):
+            
+            if beta_loss_for_init[i] == 'kullback-leibler':
+                solver_for_init[i] = 'mu'
+                
+            print('The parameters selected for run' + str([i]) + ' are ' + str(init_for_init[i]) + ', ' + str(solver_for_init[i]) + ', ' + str(beta_loss_for_init[i]) + ', ' + str(tol_for_init[i]) + ', ' + str(max_iter_for_init[i]) + ', ' + str(shuffle_for_init[i]) + '. \n' )
+            weights, components, beta = Run_NMF(Data, init = init_for_init[i], solver = solver_for_init[i], 
+                                                beta_loss = beta_loss_for_init[i], itear = max_iter_for_init[i], show = False)
+            
+            beta_div.append(beta)
+        beta_np = np.array(beta_div)
+        good_init = np.argmin(beta_np)
+        
+        weights, components, beta = Run_NMF(Data, show = True, init = init_for_init[good_init], solver = solver_for_init[good_init], beta_loss=beta_loss_for_init[good_init],
+                                             itear = max_iter_for_init[good_init])
+        
+        AggComponents = np.array(components).T
+        AggClusters, found_compos = AggCluster(clusters, AggComponents)
+        
+        found_compos = np.array(found_compos)
+            
+        
+    return found_compos
