649c9b0a87a19900d10a3b603d0f8409e4aa8361
dalverson@ufl.edu <dalverson@ufl.edu>
2024-12-17 13:31:48 -0500

    update setup.py

--- IsoDAT2D/setup.py
+++ IsoDAT2D/setup.py
@@ -8,6 +8,6 @@ setup(
     long_description= 'Combining non-negative matrix factorization and clustering algorithms to process thin film X-ray total scattering data and separate isotropic and anisotropic components',
     author_email='dalverson@ufl.edu',
     packages = ['IsoDAT2D',],
-    url='https://github.com/dnalverson/IsoDAT2D'
+    url='https://github.com/dnalverson/IsoDAT2D',
     install_requires = ['matplotlib', 'pyFAI', 'scipy', 'numpy', 'scikit-learn']
 )
